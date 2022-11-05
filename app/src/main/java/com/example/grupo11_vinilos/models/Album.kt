8c5673f3295aac3089d4b915792f8bec85979537
Luis Guzman <lm.guzmanp1@uniandes.edu.co>
2022-11-04 20:33:42 -0500

    Add NetworkServiceAdapter.kt and Album class
    Signed-off-by: Luis Guzman <lm.guzmanp1@uniandes.edu.co>

---
+++ app/src/main/java/com/example/grupo11_vinilos/models/Album.kt
@@ -0,0 +1,11 @@
+package com.example.grupo11_vinilos.models
+
+data class Album(
+    val albumId: Int,
+    val name: String,
+    val cover: String,
+    val releaseDate: String,
+    val description: String,
+    val genre: String,
+    val recordLabel: String
+)
---
+++ app/src/main/java/com/example/grupo11_vinilos/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.grupo11_vinilos.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.grupo11_vinilos.R
+import com.example.grupo11_vinilos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // Make sure actions in the ActionBar get propagated to the NavController
+        Log.d("act", navController.toString())
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
