f427f15f09208ae54fd84e4bc73c94ab476821b3
diego.eslava <d.eslava@uniandes.edu.co>
2022-11-06 13:38:50 -0500

    MusicianFragmentTest added

---
+++ app/src/androidTest/java/com/example/grupo11_vinilos/ui/MusicianFragmentTest.kt
@@ -0,0 +1,38 @@
+package com.example.grupo11_vinilos.ui
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.grupo11_vinilos.R
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+public class MusicianFragmentTest {
+    @Rule
+    @JvmField
+    var activityRule = ActivityScenarioRule<MainActivity>(
+        MainActivity::class.java
+    )
+
+    @Test
+    fun musician_name_is_displayed() {
+        onView(allOf(withId(R.id.musicianName), isDisplayed()))
+    }
+
+    @Test
+    fun musician_birthdate_is_displayed() {
+        onView(allOf(withId(R.id.musicianBirthDate), isDisplayed()))
+    }
+
+    @Test
+    fun musician_portrait_is_displayed() {
+        onView(allOf(withId(R.id.musicianPortrait), isCompletelyDisplayed()))
+    }
+}
