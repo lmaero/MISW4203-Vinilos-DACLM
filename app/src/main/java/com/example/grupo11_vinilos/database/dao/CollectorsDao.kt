0dc4cc91cdd161f1ffa301bc261ab0ffbc65141d
Luis Guzman <lm.guzmanp1@uniandes.edu.co>
2022-11-19 21:03:41 -0500

    Add Insert and DELETE methods for DAOs
    Signed-off-by: Luis Guzman <lm.guzmanp1@uniandes.edu.co>

--- app/src/main/java/com/example/grupo11_vinilos/database/dao/CollectorsDao.kt
+++ app/src/main/java/com/example/grupo11_vinilos/database/dao/CollectorsDao.kt
@@ -1,6 +1,8 @@
 package com.example.grupo11_vinilos.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.grupo11_vinilos.models.Collector
 
@@ -8,4 +10,10 @@ import com.example.grupo11_vinilos.models.Collector
 interface CollectorsDao {
     @Query("SELECT * FROM collectors_table")
     fun getCollectors(): List<Collector>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(collector: Collector)
+
+    @Query("DELETE FROM collectors_table")
+    suspend fun deleteAll()
 }
--- app/src/main/java/com/example/grupo11_vinilos/database/dao/MusiciansDao.kt
+++ app/src/main/java/com/example/grupo11_vinilos/database/dao/MusiciansDao.kt
@@ -1,6 +1,8 @@
 package com.example.grupo11_vinilos.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.grupo11_vinilos.models.Musician
 
@@ -8,4 +10,10 @@ import com.example.grupo11_vinilos.models.Musician
 interface MusiciansDao {
     @Query("SELECT * FROM musicians_table")
     fun getMusicians(): List<Musician>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(musician: Musician)
+
+    @Query("DELETE FROM musicians_table")
+    suspend fun deleteAll()
 }
