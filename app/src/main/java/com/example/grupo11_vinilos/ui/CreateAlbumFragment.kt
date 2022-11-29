1011745bfebafe61f6df639cfe7a0f7b636197f1
Alonso Cantu <a.cantu@uniandes.edu.co>
2022-11-29 00:17:59 -0600

    feature/HU07: Get the album information when the save button is pressed

--- app/src/main/java/com/example/grupo11_vinilos/ui/CreateAlbumFragment.kt
+++ app/src/main/java/com/example/grupo11_vinilos/ui/CreateAlbumFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import com.example.grupo11_vinilos.R
 import com.example.grupo11_vinilos.viewmodels.CreateAlbumViewModel
 
@@ -21,9 +22,22 @@ class CreateAlbumFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.create_album_fragment, container, false)
+
+        val name = view.findViewById<EditText>(R.id.createAlbumName)
+        val cover = view.findViewById<EditText>(R.id.createAlbumCover)
+        val releaseDate = view.findViewById<EditText>(R.id.createAlbumReleaseDate)
+        val genre = view.findViewById<EditText>(R.id.createAlbumGenre)
+        val recordLabel = view.findViewById<EditText>(R.id.createAlbumRecordLabel)
+        val description = view.findViewById<EditText>(R.id.createAlbumDescription)
         val saveButton = view.findViewById<Button>(R.id.buttonSaveAlbumInformation)
+
         saveButton.setOnClickListener{
-            Log.d("SaveButton", "Save button was pressed")
+            Log.d("name", name.text.toString())
+            Log.d("cover", cover.text.toString())
+            Log.d("releaseDate", releaseDate.text.toString())
+            Log.d("genre", genre.text.toString())
+            Log.d("recordLabel", recordLabel.text.toString())
+            Log.d("description", description.text.toString())
         }
         return view
     }
