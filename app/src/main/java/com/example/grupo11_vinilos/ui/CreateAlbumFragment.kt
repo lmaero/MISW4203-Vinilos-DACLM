f9c850957156010790beb6e93d13b03530d8e3db
Alonso Cantu <a.cantu@uniandes.edu.co>
2022-11-28 23:40:54 -0600

    feature/HU07: Add the layout to get the album information and implement the save button on click listener

--- app/src/main/java/com/example/grupo11_vinilos/ui/CreateAlbumFragment.kt
+++ app/src/main/java/com/example/grupo11_vinilos/ui/CreateAlbumFragment.kt
@@ -2,10 +2,12 @@ package com.example.grupo11_vinilos.ui
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.grupo11_vinilos.R
 import com.example.grupo11_vinilos.viewmodels.CreateAlbumViewModel
 
@@ -17,11 +19,13 @@ class CreateAlbumFragment : Fragment() {
 
     private lateinit var viewModel: CreateAlbumViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.create_album_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.create_album_fragment, container, false)
+        val saveButton = view.findViewById<Button>(R.id.buttonSaveAlbumInformation)
+        saveButton.setOnClickListener{
+            Log.d("SaveButton", "Save button was pressed")
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
