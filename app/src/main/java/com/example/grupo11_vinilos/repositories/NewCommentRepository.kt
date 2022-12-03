52365b21bbb4c241ff58307a71a7b75154f250c4
Camilo Gálvez Vidal <c.galvezv@uniandes.edu.co>
2022-12-02 23:26:09 -0300

    feature/HU09 Add new comment repository

---
+++ app/src/main/java/com/example/grupo11_vinilos/repositories/NewCommentRepository.kt
@@ -0,0 +1,11 @@
+package com.example.grupo11_vinilos.repositories
+
+import android.app.Application
+import com.example.grupo11_vinilos.network.NetworkServiceAdapter
+import org.json.JSONObject
+
+class NewCommentRepository (val application: Application) {
+    fun refreshData(body: JSONObject, albumId: Int) {
+        NetworkServiceAdapter.getInstance(application).postComment(body, albumId)
+    }
+}
