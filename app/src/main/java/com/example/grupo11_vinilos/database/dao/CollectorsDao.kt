60278efdee18382cf9b86e13f94dff7e83d40b08
Luis Guzman <luis.dev@lmaero.pro>
2022-12-03 11:16:35 -0500

    Removed unnecessary methods and optimize imports (LINT)

--- app/src/main/java/com/example/grupo11_vinilos/database/dao/CollectorsDao.kt
+++ app/src/main/java/com/example/grupo11_vinilos/database/dao/CollectorsDao.kt
@@ -1,8 +1,6 @@
 package com.example.grupo11_vinilos.database.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.grupo11_vinilos.models.Collector
 
@@ -10,10 +8,4 @@ import com.example.grupo11_vinilos.models.Collector
 interface CollectorsDao {
     @Query("SELECT * FROM collectors_table")
     fun getCollectors(): List<Collector>
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(collector: Collector)
-
-    @Query("DELETE FROM collectors_table")
-    suspend fun deleteAll()
 }
